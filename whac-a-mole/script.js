bd34db0f627e4fb6c19df0f8f4a615ecae6ceb9a
ZuzannaPaw <zuzannapaw@wp.pl>
2023-08-03 16:28:39 +0200

    feat: whac-a-mole

---
+++ whac-a-mole/script.js
@@ -0,0 +1,51 @@
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const timeLeft = document.getElementById("time-left");
+
+const score = document.getElementById("score");
+
+let result = 0;
+let hitPosition;
+let time = 60;
+let movingMole;
+let timeLeftCounter;
+
+const randomSquare = () => {
+  squares.forEach((square) => {
+    square.classList.remove("mole");
+  });
+
+  let randomSquareForMole = squares[Math.floor(Math.random() * 9)];
+  randomSquareForMole.classList.add("mole");
+
+  hitPosition = randomSquareForMole.id;
+};
+
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitPosition) {
+      result++;
+      score.textContent = result;
+    }
+  });
+});
+
+const moveMole = () => {
+  movingMole = setInterval(randomSquare, 500);
+};
+
+moveMole();
+
+function countDown() {
+  time--;
+  timeLeft.innerText = time;
+  if (time === 0) {
+    clearInterval(timeLeftCounter);
+    clearInterval(movingMole);
+    alert(`GAME OVER! Your final score is ${result}`);
+  }
+}
+
+// const counter = setInterval(countDown, 300);
+
+timeLeftCounter = setInterval(countDown, 1000);
